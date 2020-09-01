aabf8929fdad040790da8cff6f7ab92b56b4ce45
Kwon770 <sckwon770@gmail.com>
2020-09-01 10:30:37 +0900

    feat: Add client socket code to connect backend

--- with_socket.io/client/src/component/Chat/Chat.js
+++ with_socket.io/client/src/component/Chat/Chat.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+// location comes from react router, given as props
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    // parse data from path(url) [return as Object]
+    const { name, room } = queryString.parse(location.search);
+
+    // Create client socket
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    // Emit message with data to backend
+    // (MESSAGE, DATA)
+    socket.emit("join", { name, room });
+  }, [ENDPOINT, location.search]);
+
   return <h1>Chat</h1>;
 };
 
